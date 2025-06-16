33d7b66b0b01fe3324e52a777925bf6e5333f423
SeanJeonghwanLee <ljhwan1997@naver.com>
2025-06-16 20:09:32 +0900

    [TEST] on test

--- src/cocktail_robot/cocktail_robot/stir_and_garnish/stir.py
+++ src/cocktail_robot/cocktail_robot/stir_and_garnish/stir.py
@@ -1,27 +1,47 @@
 # pick and place in 1 method. from pos1 to pos2 @20241104
+import DR_init
 import time
-import rclpy
-from DSR_ROBOT2 import DR_MV_MOD_REL, DR_BASE, DR_AXIS_Z, DR_QSTOP, DR_TOOL
-from DR_common2 import posx, posj
 from ..utils.base_action import BaseAction
 
 
 
+ON, OFF = 1, 0
 ### 힘 제어 : BASE 좌표계 기준
 class StirAction(BaseAction):
-    def __init__(self, arm, stir_pose):
-        self.arm = arm
+    def __init__(self, node, stir_pose):
+        DR_init.__dsr__node = node
+        try:
+            from DSR_ROBOT2 import (
+                release_compliance_ctrl,
+                check_force_condition,
+                task_compliance_ctrl,
+                set_desired_force,
+                set_tool,
+                set_tcp,
+                movej,
+                movel,
+                DR_FC_MOD_REL,
+                DR_AXIS_Z,
+                DR_BASE,
+            )
+            from DR_common2 import posx
+
+        except ImportError as e:
+            print(f"Error importing DSR_ROBOT2 : {e}")
+            return
+        
         self.stir_pose = stir_pose
+        self.grasp_option = 1
 
     def execute(self):
-        self.arm.movej(self.stir_pose["spoon_grasp_0"]["joint"])
-        self.arm.movej(self.stir_pose["spoon_grasp_1"]["joint"])
-        self.arm.grasp()
-        self.arm.movel(pos=[0,0,160,0,0,0], mod=DR_MV_MOD_REL, ref=DR_BASE)
-        self.arm.movej(self.stir_pose["stir"]["joint"])
+        movej(self.stir_pose["spoon_grasp_0"]["joint"])
+        movej(self.stir_pose["spoon_grasp_1"]["joint"])
+        self.grasp(self.grasp_option)
+        movel(pos=[0,0,160,0,0,0], mod=DR_MV_MOD_REL, ref=DR_BASE)
+        movej(self.stir_pose["stir"]["joint"])
         self.down_stir_up()
-        self.arm.movej(self.stir_pose["spoon_grasp_1"]["joint"])
-        self.arm.movej(self.stir_pose["spoon_grasp_0"]["joint"])
+        movej(self.stir_pose["spoon_grasp_1"]["joint"])
+        movej(self.stir_pose["spoon_grasp_0"]["joint"])
 
 
     def down_stir_up(self, target_pos=336.4, turning_radius=10, stir_repeat=10, return_posx=100, force_desired=20):
@@ -29,24 +49,40 @@ class StirAction(BaseAction):
         f_d = [0.0, 0.0, -force_desired, 0.0, 0.0, 0.0]
         f_dir = [0, 0, 1, 0, 0, 0]
         
-        self.arm.task_compliance_ctrl(k_d)
-        self.arm.set_desired_force(f_d, f_dir)
+        task_compliance_ctrl(k_d)
+        set_desired_force(f_d, f_dir)
 
         while True:
-            if not self.arm.check_position_condition(axis=DR_AXIS_Z, max=target_pos, ref=DR_BASE):
+            if not check_position_condition(axis=DR_AXIS_Z, max=target_pos, ref=DR_BASE):
                 time.sleep(0.5)
-                self.arm.release_force(time=0.5)
-                self.arm.release_compliance_ctrl()
+                release_force(time=0.5)
+                release_compliance_ctrl()
                 break       
             else:
                 pass   
                 
-        self.arm.move_periodic(
+        move_periodic(
             amp=[turning_radius, turning_radius, 0, 0, 0, 0],
             period=[1, 1, 0, 0, 0, 0],
             repeat=stir_repeat,
             ref=DR_BASE
             )
         
-        self.arm.movej(pos=posx(0,0,100,0,0,0), ref=DR_BASE, mod=DR_MV_MOD_REL)
+        movej(pos=posx(0,0,100,0,0,0), ref=DR_BASE, mod=DR_MV_MOD_REL)
+
+    def grasp(self, x):
+        self._set_custom_grasp(x)
+        set_digital_output(1, ON)
+        wait(0.5)
+
+    def release(self, x):
+        self._set_custom_grasp(x)
+        set_digital_output(1, OFF)
+        wait(0.5)
+
+    def _set_custom_grasp(self, x):
+        if x == 0:
+            set_digital_output(2, OFF)
+        elif x == 1:
+            set_digital_output(2, ON)
         
