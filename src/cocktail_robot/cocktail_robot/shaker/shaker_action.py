4bca2ac1b4935372df3953d138426ed38d3a91de
Rebi00dev <skfkdRp489@gmail.com>
2025-06-17 20:12:22 +0900

    [UPDATE] complete shaker action

--- src/cocktail_robot/cocktail_robot/shaker/shaker_action.py
+++ src/cocktail_robot/cocktail_robot/shaker/shaker_action.py
@@ -2,9 +2,9 @@
 from ..utils.base_action import BaseAction
 import DR_init
 import time
-
+ON, OFF = 1, 0
 DR = None
-
+VEL, ACC = 100, 30
 class ShakerAction(BaseAction):
     def __init__(self, node, poses):
         DR_init.__dsr__node = node
@@ -17,34 +17,42 @@ class ShakerAction(BaseAction):
             return
 
     def execute(self):
-        DR.set_velx(vel1=100,vel2=50)
-        DR.set_accx(30, 30)
         pick_poses = self.poses['pick']
         pick_before_poses = self.poses['pick_before']
         shaking_poses = self.poses['shaking']
         place_before_poses = self.poses["place_before"]
         # 1. 잡는 위치로 이동
-        DR.movej([0,0,90,0,90,0])
-        DR.movel(pick_before_poses['task'])
-        DR.movel(pick_poses['task'])
+        DR.movej([0,0,90,0,90,0],vel=VEL,acc=ACC)
+        DR.movel(pick_before_poses['task'],vel=VEL,acc=ACC)
+        DR.movel(pick_poses['task'],vel=VEL,acc=ACC)
         self.grasp()
-        
-        DR.movel(shaking_poses['task'])
-        # DR.movej([0, -30, 90, 0, 60, 0])
-        # DR.movej([10, -20, 100, 0, 70, 10])
-        # DR.movej([-10, -40, 110, 0, 50, -10])
-        DR.amove_periodic([50,50,0,0,0,15],[1,1.5,0,0,0,1])
-        # 2. 충돌 방지 경로
-        DR.movel(shaking_poses['task'])
-        DR.movel(place_before_poses['task'])
-        # 3. 내려놓기
-        DR.movel(pick_poses['task'])
-        self.release()
-        DR.movel(pick_before_poses['task'])
-        
-        
-    def grasp():
-        pass
 
-    def release():
-        pass
+        DR.movel(place_before_poses['task'],vel=VEL,acc=ACC)
+        DR.movel(shaking_poses['task'],vel=VEL,acc=ACC,ref=DR.DR_BASE)
+
+        DR.move_periodic([50, 50, 0, 0, 0, 45], [1.25,0.75, 0, 0, 0, 2],repeat=10,atime=0.1,ref=DR.DR_TOOL)
+        # 2. 충돌 방지 경로
+        DR.movel(shaking_poses['task'],vel=VEL,acc=ACC)
+        DR.movel(place_before_poses['task'],vel=VEL,acc=ACC)
+        # 3. 내려놓기
+        DR.movel(pick_poses['task'],vel=VEL,acc=ACC)
+        self.release()
+        DR.movel(pick_before_poses['task'],vel=VEL,acc=ACC)
+        DR.movel(place_before_poses['task'],vel=VEL,acc=ACC)
+        home_pose = place_before_poses['task'].copy()
+        home_pose[3:] = [0,-180,0]
+        DR.movel(home_pose,vel=VEL,acc=ACC)
+        DR.movej([0,0,90,0,90,0],vel=VEL,acc=ACC)
+        
+        
+    def grasp(self):
+        DR.set_digital_output(1,ON)
+        DR.set_digital_output(2,OFF)
+        DR.wait(1)
+
+    def release(self):
+        DR.set_digital_output(1,OFF)
+        DR.set_digital_output(2,OFF)
+        DR.wait(1)
+
+        
